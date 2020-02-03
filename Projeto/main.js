ef2fee4cd00927f0fede8789bb83ca6af5c0db04
Matheus Henrique Santiro Lucena <matheus.lucena@enext.local>
2020-02-03 16:33:08 -0300

    insertion new page, new tools page travel

--- Projeto/main.js
+++ Projeto/main.js
@@ -1,3 +1,5 @@
+/*Modal de login*/
+
 var modal = document.getElementById('id01');
 
 window.onclick = function(event) {
@@ -7,6 +9,34 @@ window.onclick = function(event) {
 }
 function pagInicial(){
 
-        window.location.replace("main_responsavel.html")
+        window.location.href = "../travel/main_responsavel.html";
     
 }
+
+
+/*Slides Quem somos*/
+
+
+var myIndex = 0;
+slides();
+
+function slides() {
+  var x = document.getElementsByClassName("slide-show");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(slides, 1000); 
+}
+
+
+/*Função alert do fale conosco*/
+
+function enviarEmail(){
+
+    alert("Te encaminhamos uma mensagem no seu e-mail, aguarde um instante");
+}
+
+
